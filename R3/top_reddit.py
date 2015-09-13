7d570062896ea5cbe0d85a7c0512001332a838df
LucasNeis <lucasneis@hotmail.com.br>
2015-09-13 00:13:29 -0300

    first commit

---
+++ R3/top_reddit.py
@@ -0,0 +1,20 @@
+import urllib, json, sys
+
+sub = sys.argv
+if len(sys.argv) == 1:
+   print "\nYou must insert a subreddit name. Example: phyton top_reddit.py pcmasterrace\n"
+   sys.exit(0)
+
+hot = json.loads(urllib.urlopen("http://reddit.com/r/" + sub[1] + "/hot.json").read())
+
+try:
+    error = hot['error']
+    if error:
+        print "\nSub not found\n"
+        sys.exit(0)
+except:
+    print ""
+
+for i in range(10):
+    data = hot["data"]["children"][i]["data"]
+    print "Post: " + data["title"] + "\nKarma: " + str(data["score"]) + "\npost url: www.reddit.com" + data["permalink"] + "\nlink: " + data["url"]+ "\n"
